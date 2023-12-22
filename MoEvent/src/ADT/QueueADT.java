26268254450bcef8eeb460e093bdbf3ab7f88ab8
Bui Quang Minh <119041798+Bminh1709@users.noreply.github.com>
2023-12-22 21:50:36 +0700

    Delete MoEvent directory

--- MoEvent/src/ADT/QueueADT.java
+++
@@ -1,9 +0,0 @@
-package ADT;
-
-public interface QueueADT <E> extends Iterable<E> {
-    void offer(E element);
-    E poll();
-    E peek();
-    int size();
-    boolean isEmpty();
-}
--- MoEvent/src/ADT/StackADT.java
+++
@@ -1,9 +0,0 @@
-package ADT;
-
-public interface StackADT<E> extends Iterable<E> {
-    void push(E element);
-    E pop();
-    E peek();
-    int size();
-    boolean isEmpty();
-}
